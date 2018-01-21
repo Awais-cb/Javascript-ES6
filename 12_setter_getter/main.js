c44bd5d9bdad0fdf43d5498abad830e63a8a12bd
Awais Fiaz <awaisfiazit@gmail.com>
2018-01-22 01:38:45 +0500

    Added :Higher order functions with arrays

---
+++ 12_setter_getter/main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+/*
+getter and setter are basically used to implement computed properties.
+*/
+function demo (fname,lname) {
+	this.firstname=fname;
+	this.lastname=lname;
+}
+
+demo.prototype{
+
+
+	this.firstname;
+	set fullName(){
+		
+	}
+
+}
